d015688205980c161ea4854951fc57a15d29a344
Razor <noprantor@gmail.com>
2017-12-07 09:55:00 +0700

    Version 3

--- app/src/main/java/com/example/razor/implicit_intent/MainActivity.java
+++ app/src/main/java/com/example/razor/implicit_intent/MainActivity.java
@@ -1,29 +1,22 @@
 package com.example.razor.implicit_intent;
 
-import android.Manifest;
-import android.content.ComponentName;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.Uri;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.ShareCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.telephony.SmsManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+  //Inisialisasi Variabel
     private EditText mWebsiteEditText;
     private EditText mshareEditText;
     private EditText mcallEditText;
     private EditText msmsEditText;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,15 +28,16 @@ public class MainActivity extends AppCompatActivity {
         msmsEditText = (EditText) findViewById(R.id.edtsms);
     }
 
+    //Fungsi yang digunakan pada website
     public void openwebsite(View view) {
-        // Get the URL text.
+        // Mendapatkan URL dari Edit Text
         String url = "http://" + (mWebsiteEditText.getText().toString());
 
-        // Parse the URI and create the intent.
+        // Parse nilai uri ke url.
         Uri webpage = Uri.parse(url);
         Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
 
-        // Find an activity to hand the intent and start that activity.
+       //menampilkan activity sesuai dengan kondisi
         if (intent.resolveActivity(getPackageManager()) != null) {
             startActivity(intent);
         } else {
@@ -51,8 +45,12 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    //Fungsi yang digunakan untuk share
     public void texhshare(View view) {
+        //Mendapatkan text dari edittext
         String txt = mshareEditText.getText().toString();
+
+        //fungsi intent builder digunakan untuk memilih target intent pada aplikasi lain
         ShareCompat.IntentBuilder
                 .from(this)
                 .setType("text/plain")
@@ -60,44 +58,42 @@ public class MainActivity extends AppCompatActivity {
                 .setText(txt)
                 .startChooser();
     }
-    //private static final int CALL = 1;
 
+    //Fungsi yang digunakan untuk call/dial
     public void phonecall(View view) {
-
+        //memformat hasil inputan user
         String nomortelp = String.format("tel: %s", mcallEditText.getText().toString()) ;
+        //mendefinisikan intent ke action_dial
         Intent dialIntent = new Intent(Intent.ACTION_DIAL);
+        //parse uri data dari inputan user
         dialIntent.setData(Uri.parse(nomortelp));
+        //pengecekan status
         if (dialIntent.resolveActivity(getPackageManager()) != null) {
             startActivity(dialIntent);
         } else {
             Log.d("phonecall:", "cannot call this number");
         }
 
-
-   /*     if (ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
-            ActivityCompat.requestPermissions(MainActivity.this, new String[] { Manifest.permission.CALL_PHONE},CALL);
-        }else {
-            Intent intentphone = new Intent(Intent.ACTION_CALL);
-
-            intentphone.setData(Uri.parse("tel:085320288825"));
-            startActivity(intentphone);
-        }
-*/
-
     }
 
+    //fungsi yang digunakan untuk mengirim sms
     public void sendsms(View view) {
+        //memformat nomor sms
         String smsNumber = String.format("smsto: %s", mcallEditText.getText().toString());
+        //menampung string inputan ke string sms
         String sms = msmsEditText.getText().toString();
+        //mendefinisikan intent ke ACTION_SENDTO
         Intent smsIntent = new Intent(Intent.ACTION_SENDTO);
+        //parse data
         smsIntent.setData(Uri.parse(smsNumber));
+        //memasukkan nilai dari string sms ke SMS BODY
         smsIntent.putExtra("sms_body", sms);
+        //Cek Status
         if (smsIntent.resolveActivity(getPackageManager()) != null) {
             startActivity(smsIntent);
         } else {
             Log.d("SMS:", "Can't resolve app for ACTION_SENDTO Intent.");
         }
     }
-
 
 }
